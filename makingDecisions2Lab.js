306696fccd6515c9c43bf71cdbd44da6c51c8169
Blake Pond <blakehp@icloud.com>
2021-10-14 14:13:00 -0600

    problem 2 solved

--- makingDecisions2Lab.js
+++ makingDecisions2Lab.js
@@ -9,13 +9,15 @@ var arr = [10,20,30,40,50,60]
 let firstItem = arr[0] 
   console.log(firstItem)
 
-
 ////////// PROBLEM 2 //////////
 /*
-  Remove the last item from the 'arr' array and store it in a variable named 'lastItemRemoved'. Console.log the array to verify you no longer have the last item.
+  Remove the last item from the 'arr' array and store it in a variable named 'lastItemRemoved'. 
+  Console.log the array to verify you no longer have the last item.
 */
 
-//Code Here
+
+let lastItemRemoved = arr.pop()
+  console.log(lastItemRemoved)
 
 ////////// PROBLEM 3 //////////
 
