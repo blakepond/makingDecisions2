4b7c83440ff412046dd63b08b57c8944bacb7948
Blake Pond <blakehp@icloud.com>
2021-10-14 16:15:02 -0600

    Problem7 complete

--- makingDecisions2Lab.js
+++ makingDecisions2Lab.js
@@ -86,25 +86,34 @@ var score = 74
 ////////// PROBLEM 6 //////////
 
 // Do not edit the code below.
-var myFavoriteNumbers = [4,8,12,16,20,24];
+var myFavoriteNumbers = [4,8,12,16,20,24,10,19,33];
 // Do not edit the code above.
 
 /*
   Create a variable named 'someNum' and set it equal to the fifth value of the 'myFavoriteNumbers' array.
 */
 
-var someNum = myFavoriteNumbers.slice(4,myFavoriteNumbers.length -1) 
-  console.log(someNum)
+// var someNum = myFavoriteNumbers.slice(4,myFavoriteNumbers.length -1) 
+//   console.log(someNum)
 
 
 
 ////////// PROBLEM 7 //////////
 
-// Subscripting (accessing values using their index) an array can fail. What happens if you subscript to the 7th element, but there are not 7 elements in the array? Let's write some code to check for that. 
-// Use an if statement to check the length of the 'myFavoriteNumbers' array. If it's less than 7, log 'There are not enough elements in this array' to the console. If the length is more than 7, reassign the value of 'someNum' to the value of the 7th element in the array. (Hint: how can you make sure that your code works for exactly 7 elements? What index do you use to get the 7th element?)
+// // Subscripting (accessing values using their index) an array can fail. 
+// What happens if you subscript to the 7th element, but there are not 7 elements in the array? 
+// Let's write some code to check for that. 
+// // Use an if statement to check the length of the 'myFavoriteNumbers' array. 
+// If it's less than 7, log 'There are not enough elements in this array' to the console. 
+// If the length is more than 7, reassign the value of 'someNum' to the value of the 7th element in the array. 
+// (Hint: how can you make sure that your code works for exactly 7 elements? What index do you use to get the 7th element?)
 
-//Code Here
-
+if (myFavoriteNumbers.length < 7) {
+console.log ("there are not enought elements in this array")
+} else {(myFavoriteNumbers.length === 7) 
+  someNum = myFavoriteNumbers.splice(6, 1)
+  console.log(someNum)
+}
 
 ////////// PROBLEM 8 //////////
 
@@ -112,7 +121,8 @@ var someNum = myFavoriteNumbers.slice(4,myFavoriteNumbers.length -1)
 var listOfNumbers = [1,2,3,4,5,6,7,8,9,10,11,12];
 // Do not edit the code above.
 
-// Use a for-loop to iterate through 'listOfNumbers', checking to see if each number is divisible by 3. If it is, console.log '{number} is divisible by 3.'
+// // Use a for-loop to iterate through 'listOfNumbers', checking to see if each number is divisible by 3.
+//  If it is, console.log '{number} is divisible by 3.'
 
 //Code Here
 
