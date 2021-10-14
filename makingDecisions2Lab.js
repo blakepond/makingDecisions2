b75ec56acb09913021b5de0bf37fb9f18008e7e9
Blake Pond <blakehp@icloud.com>
2021-10-14 16:34:33 -0600

    problem8 complete

--- makingDecisions2Lab.js
+++ makingDecisions2Lab.js
@@ -108,12 +108,12 @@ var myFavoriteNumbers = [4,8,12,16,20,24,10,19,33];
 // If the length is more than 7, reassign the value of 'someNum' to the value of the 7th element in the array. 
 // (Hint: how can you make sure that your code works for exactly 7 elements? What index do you use to get the 7th element?)
 
-if (myFavoriteNumbers.length < 7) {
-console.log ("there are not enought elements in this array")
-} else {(myFavoriteNumbers.length === 7) 
-  someNum = myFavoriteNumbers.splice(6, 1)
-  console.log(someNum)
-}
+// if (myFavoriteNumbers.length < 7) {
+// console.log ("there are not enought elements in this array")
+// } else {(myFavoriteNumbers.length === 7) 
+//   someNum = myFavoriteNumbers.splice(6, 1)
+//   console.log(someNum)
+// }
 
 ////////// PROBLEM 8 //////////
 
@@ -124,7 +124,14 @@ var listOfNumbers = [1,2,3,4,5,6,7,8,9,10,11,12];
 // // Use a for-loop to iterate through 'listOfNumbers', checking to see if each number is divisible by 3.
 //  If it is, console.log '{number} is divisible by 3.'
 
-//Code Here
+for (let i = 0; i < listOfNumbers.length; i++) 
+    if (listOfNumbers[i] % 3 === 0) {
+      console.log(listOfNumbers[i] + " is divisible by 3")
+
+}
+
+
+
 
 
 ////////// PROBLEM 9 //////////
