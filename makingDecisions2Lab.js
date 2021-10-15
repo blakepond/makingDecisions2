5b376470173f4869c70163facd382596d960245b
Blake Pond <blakehp@icloud.com>
2021-10-14 18:07:45 -0600

    Problem 10 complete

--- makingDecisions2Lab.js
+++ makingDecisions2Lab.js
@@ -146,15 +146,16 @@ var letters = ['A', 'B', 'C', 'D', 'E'];
 //creat for loop and assign first index to last
 
 
-for (let i = letters.length -1; i >= 0; i--) {
-    console.log(letters[i])
-}
+// for (let i = letters.length -1; i >= 0; i--) {
+//     console.log(letters[i])
+// }
 
 
 ////////// Advanced Problems //////////
 
 ////////// PROBLEM 10 //////////
-// Switch statements can be excellent alternatives to if blocks. Look up switch statements (I recommend W3 Schools) and try to implement one for the following.
+// Switch statements can be excellent alternatives to if blocks. 
+// Look up switch statements (I recommend W3 Schools) and try to implement one for the following.
 
 // Do not edit the code below.
 let letterGrade = 'B'
@@ -170,8 +171,26 @@ If F: "The student is failing."
 If the letter grade is not one of the above letters, console.log 'Not an eligible grade.'
 */
 
-//Code Here
+switch (letterGrade) {
+    case "A":
+      console.log("The student is doing excellently.");
+        break;
+    case "B":
+      console.log("The student is doing well.");
+        break;
+    case "C":
+      console.log("The student is doing alright.");
+        break;
+    case "D":
+      console.log("The student is not doing very well.");
+        break;
+    case "F":
+      console.log("The student is failing.");
+        break;
+    default:
+        console.log("Not an eligible grade.");
 
+}
 
 
 ////////// PROBLEM 11 //////////
